2fbdc1376f8b8c4e17c19a6887719e0ff2acb239
weain <7495171+weain@user.noreply.git.com>
2020-04-25 22:57:30 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configurableWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}
